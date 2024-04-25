7bb852adfdca47946412ae66d2248e31af4a2047
JArabiaT <jordi.arabia24@gmail.com>
2024-04-25 11:41:29 +0200

    Create misFunciones.py

---
+++ misFunciones.py
@@ -0,0 +1,20 @@
+import os
+def cls():
+    clear = lambda: os.system('cls')
+    clear()
+
+def mostrar_menu():
+    print("  ──  ── El Libro de las Accepciónes ──  ──  ")
+    print("─────────────────────────────────────────────")
+    print(" 1- Buscar en el diccionario")
+    print(" 2- Añadir palabra")
+    print(" 3- Eliminar palabra")
+    print(" 4- Salir")
+
+def mostrar_BuscaMenu():
+    print("  ──  ──  Buscar en el diccionario  ──  ──   ")
+    print("─────────────────────────────────────────────")
+    print(" 1- Buscar por palabra clave")
+    print(" 2- ")
+
+
